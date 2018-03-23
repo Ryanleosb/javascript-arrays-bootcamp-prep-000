84ba1e7f694a77fcc3626fc96b67d788397e29d8
leo bang <ryanleosb@gmail.com>
2018-03-23 03:59:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,3 +22,23 @@ function destructivelyAddElementToEndOfArray (array, newElement) {
 function accessElementInArray (array,index){
   return array[index];
 };
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  array.shift();
+  return array;
+};
+
+function removeElementFromBeginningOfArray(array) {
+  var newArray = array.slice(1)
+  return newArray;
+};
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+};
+
+function removeElementFromEndOfArray(array) {
+  var newArray = array.slice (0, array.length -1);
+  return newArray;
+}
